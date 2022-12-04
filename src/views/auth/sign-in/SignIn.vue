c8ab96716cdd9541c1de10a8ef8b304417cdd369
MadoPlus <ma7modmado@gmail.com>
2022-12-04 11:59:02 +0200

    Added Routing for all authentication

---
+++ src/views/auth/sign-in/SignIn.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Sign in Component</h1>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+};
+</script>
---
+++ src/views/auth/sign-up/SignUp.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Sign up Component</h1>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
