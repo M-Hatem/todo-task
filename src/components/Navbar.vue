98c65f3510e2e9f90ac7fbbff542053b1c056adb
MadoPlus <ma7modmado@gmail.com>
2022-12-04 17:18:16 +0200

    Added navbar component with main-links

---
+++ src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+      <RouterLink to="/">
+        <img
+          src="https://e7.pngegg.com/pngimages/629/289/png-clipart-computer-icons-checklist-icon-design-graphic-design-information-miscellaneous-text.png"
+          alt="home-icon"
+          class="home-icon"
+        />
+      </RouterLink>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Manage</a>
+          </li>
+          <li class="nav-item ms-auto">
+            <RouterLink
+              class="nav-link"
+              to="/auth/sign-in"
+              v-if="!userStore.userLoggedIn"
+            >
+              Sign In / Sign Up
+            </RouterLink>
+            <a class="nav-link" href="#" @click.prevent="signOut" v-else>
+              Sign Out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+
+import { mapStores } from "pinia";
+import useUserStore from "@/stores/user";
+
+export default {
+  name: "Navbar",
+  computed: {
+    ...mapStores(useUserStore),
+  },
+  methods: {
+    signOut() {
+      this.userStore.signOutUser();
+    },
+  },
+  // methods: {
+  //   ...mapActions(useUserStore, ["signOutUser"]),
+  //   signOut() {
+  //     signOutUser();
+  //   },
+  // },
+};
+</script>
+
+<style scoped lang="scss">
+nav {
+  .home-icon {
+    width: 30px;
+  }
+}
+</style>
