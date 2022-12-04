81adba4fc6db380e532f3471de5721ce641e9989
MadoPlus <ma7modmado@gmail.com>
2022-12-04 11:36:26 +0200

    Added favicon, website title, main styling and vee-validate package

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-// To import main.css file
-import "./assets/main.css";
 // To include bootstrap library
 import "bootstrap/dist/css/bootstrap.min.css";
+// To import main.css file
+import "./assets/main.css";
 
 const app = createApp(App);
 
