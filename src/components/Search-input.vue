9e08c16c720ea8cb6b2b1657bf78c3c0874bca87
MadoPlus <ma7modmado@gmail.com>
2022-12-05 10:16:52 +0200

    Added search functionality

---
+++ src/components/Search-input.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    type="text"
+    class="form-control my-5"
+    placeholder="Search"
+    @input="search"
+  />
+</template>
+
+<script>
+export default {
+  name: "Search-input",
+  methods: {
+    search(e) {
+      const value = e.currentTarget.value.toLowerCase();
+      this.$emit("search", value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
